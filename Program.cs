db4b68d6c41b23559e4888b9b73bd05596b669ad
KateYR3 <ekaterinavasilevich@gmail.com>
2024-12-15 01:15:36 +0200

    Inicialization_3Orders

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string? customerName;
+        string? productName;
+        double productPrice;
+        string? customerAddress;
+        int customerAdressNum;
+
+        Console.Write("Client name: ");
+        customerName = Console.ReadLine();
+        Console.Write("Product name: ");
+        productName = Console.ReadLine();
+        Console.Write("Price: ");
+        productPrice = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Client address: ");
+        customerAddress = Console.ReadLine();
+        Console.Write("Num: ");
+        customerAdressNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        string ordernumber = "Order No 1";
+        Console.WriteLine(ordernumber);
+
+        Console.WriteLine($"Client: {customerName}.");
+        Console.WriteLine($"Product: {productName}, price {productPrice} EUR.");
+        Console.WriteLine($"Address: {customerAddress}, {customerAdressNum}.");
+       
+        Console.WriteLine();
+
+        Console.Write("Client name: ");
+        customerName = Console.ReadLine();
+        Console.Write("Product name: ");
+        productName = Console.ReadLine();
+        Console.Write("Price: ");
+        productPrice = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Client adress: ");
+        customerAddress = Console.ReadLine();
+        Console.Write("Num: ");
+        customerAdressNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        var ordernumber2 = "Order No 2";
+        Console.WriteLine(ordernumber2);
+
+        Console.WriteLine($"Client: {customerName}.");
+        Console.WriteLine($"Product: {productName}, price {productPrice} EUR.");
+        Console.WriteLine($"Address: {customerAddress}, {customerAdressNum}.");
+
+        Console.WriteLine();
+
+        Console.Write("Client name: ");
+        customerName = Console.ReadLine();
+        Console.Write("Product name: ");
+        productName = Console.ReadLine();
+        Console.Write("Price: ");
+        productPrice = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Client adress: ");
+        customerAddress = Console.ReadLine();
+        Console.Write("Num: ");
+        customerAdressNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        var ordernumber3 = "Order No 3";
+        Console.WriteLine(ordernumber3);
+
+        Console.WriteLine($"Client: {customerName}. ");
+        Console.WriteLine($"Product: {productName}, price {productPrice} EUR.");
+        Console.WriteLine($"Address: {customerAddress}, {customerAdressNum}.");
+       
+        Console.WriteLine();
+    }
+}
